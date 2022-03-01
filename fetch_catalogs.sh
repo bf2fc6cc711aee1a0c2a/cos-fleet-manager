d96b92fb9dd04f8cb256136712c16c6babcfee35
Luis Garcia Acosta <lgarciaa@redhat.com>
2022-03-01 18:10:59 +0100

    feat: fetch catalogs
    Fetch catalogs, we have them all in one place, and it is easier to roll out changes to stage

---
+++ fetch_catalogs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Pull debezium and camel catalogs
+
+DEBEZIUM_CATALOG=https://raw.githubusercontent.com/bf2fc6cc711aee1a0c2a/cos-fleet-catalog-debezium/main/templates/cos-fleet-catalog-debezium.yaml
+CAMEL_CATALOGS=https://raw.githubusercontent.com/bf2fc6cc711aee1a0c2a/cos-fleet-catalog-camel/main/templates/cos-fleet-catalog-camel.yaml
+
+clear
+
+for f in $DEBEZIUM_CATALOG $CAMEL_CATALOGS; do
+  echo Downloading $f
+  curl -sS -o "templates/`basename ${f}`" ${f}
+done
